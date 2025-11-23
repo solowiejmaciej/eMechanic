c638bf360a5d5c81dd2020529d40c530423677f1
solowiejmaciej <solowiejmaciej@gmail.com>
2025-11-23 17:35:42 +0100

    feat(core): add more tests

--- src/eMechanic.Backend/Core/src/eMechanic.Application/RepairRequest/Features/Get/GetRepairRequestByIdQuery.cs
+++
@@ -1,7 +0,0 @@
-
-using eMechanic.Application.RepairRequest.Features.Get;
-using eMechanic.Common.CQRS;
-
-namespace eMechanic.Application.RepairRequest.Features.Get;
-
-public sealed record GetRepairRequestByIdQuery(Guid Id) : IResultQuery<RepairRequestResponse>;
